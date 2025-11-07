abd41e97ece6c859a272378edcce1a98f3ca1285
HusniAja211 <husnimubarakportofolio@gmail.com>
2025-11-07 20:41:31 +0700

    memasang autentikasi menggunakan laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,39 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+// Halaman utama
 Route::get('/', function () {
     return view('welcome');
 });
+
+// ===============================
+//  Group untuk admin
+// ===============================
+Route::middleware(['auth', 'verified', 'role:admin'])->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+});
+
+// ===============================
+// 🧩Group untuk petugas
+// ===============================
+Route::middleware(['auth', 'verified', 'role:petugas'])->group(function () {
+    Route::get('/petugas/dashboard', function () {
+        return view('petugas.dashboard');
+    })->name('petugas.dashboard');
+});
+
+// ===============================
+//  Profile Routes
+// ===============================
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Auth (login, register, dsb)
+require __DIR__.'/auth.php';
